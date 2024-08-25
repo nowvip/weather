bd7183c57001d8a64d791eed23e66b7c7314377d
nowvip <jxh0098@gmail.com>
2024-08-25 18:08:58 +0800

    Update list.php

--- php/list.php
+++ php/list.php
@@ -21,9 +21,9 @@ if ($jsonData === FALSE) {
 curl_close($ch);
 
 // 打印原始 JSON 数据
-echo "Raw JSON Data:\n";
-echo $jsonData;
-echo "\n";
+//echo "Raw JSON Data:\n";
+//echo $jsonData;
+//echo "\n";
 
 // 使用正则表达式提取 JSON 数据部分
 preg_match('/getLbDatas\((\{.*\})\)/', $jsonData, $matches);
@@ -41,28 +41,19 @@ if ($data === NULL) {
     die('Error decoding JSON data: ' . json_last_error_msg());
 }
 
-// 获取当前 UTC 日期
-$currentDateUTC = date('Y-m-d');
-
-// 将当前 UTC 日期转换为北京时间
-$datetime = new DateTime($currentDateUTC . ' 00:00:00', new DateTimeZone('UTC'));
-$datetime->setTimezone(new DateTimeZone('Asia/Shanghai'));
-$currentDateBeijing = $datetime->format('Y-m-d');
 
 // 处理 JSON 数据并输出符合条件的数据
 foreach ($data['data'] as $item) {
     $updateTime = $item['updateTime'];
     $itemDate = explode(' ', $updateTime)[0]; // 提取日期部分
-
-    if ($itemDate === $currentDateBeijing) {
-        $url = $item['url'];
-        $title = $item['title'];
-        
-        echo "URL: $url\n";
-        echo "Title: $title\n";
-        echo "Update Time: $updateTime\n";
-        break;
-    }
+    
+    $url = $item['url'];
+    $title = $item['title'];
+    
+    echo "$title," . "$url\n";
+    echo "Update Time: $updateTime\n";
+    break;
+    
 }
 
 }
