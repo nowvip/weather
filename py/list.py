32722acfab3d5f48ad938c7b4467c99a4e5d6f92
nowvip <jxh0098@gmail.com>
2024-08-25 16:58:50 +0800

    Update list.py

--- py/list.py
+++ py/list.py
@@ -1,15 +1,16 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
 def get_rendered_html(url):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(url)
+
+        try:
+            page.wait_for_load_state('networkidle', timeout=60000)  # 增加超时时间
+        except PlaywrightTimeoutError:
+            print("Page loading timed out.")
         
-        # 等待页面完全加载
-        page.wait_for_load_state('networkidle')
-        
-        # 获取渲染后的HTML
         rendered_html = page.content()
         print(rendered_html)
         
